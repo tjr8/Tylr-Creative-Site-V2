a0036f0bdce3838224be59ebd86ef67f4bb7c9c9
Tyler Robinson <tylr.rob@gmail.com>
2014-12-29 16:06:12 -0800

    Made owl-wedding tag in js html and css

--- js/custom.js
+++ js/custom.js
@@ -254,6 +254,17 @@ $(document).ready(function () {
     transitionStyle:"fade"
   });
 
+     $("#owl-wedding").owlCarousel({
+        autoPlay : 3000,
+        stopOnHover : true,
+        navigation:true,
+        paginationSpeed : 1000,
+        goToFirstSpeed : 2000,
+        singleItem : true,
+        autoHeight : true,
+        transitionStyle:"fade"
+    });
+
 
     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
     /* timers */
