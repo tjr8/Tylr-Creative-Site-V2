a76ef251c19c475e5486adb8634a0349fca397c4
tjr8 <tjrdesigner@gmail.com>
2016-01-23 13:29:33 -0800

    Shorten time for preload

--- js/preloader.js
+++ js/preloader.js
@@ -4,7 +4,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2014, Codrops
  * http://www.codrops.com
  */
@@ -73,12 +73,12 @@
 							onEndHeaderAnimation();
 						}
 					}
-				}, 80 );
+				}, 20 );
 		};
 
 		loader.setProgressFn( simulationFn );
 	}
-	
+
 	function noscroll() {
 		window.scrollTo( 0, 0 );
 	}
