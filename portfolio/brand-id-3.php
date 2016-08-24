d0424d324b49dba859d8e9fdf2c828fa6d9da24a
tjr8 <tjrdesigner@gmail.com>
2016-08-23 22:02:25 -0700

    Add Brad id 3

---
+++ portfolio/brand-id-3.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+
+    <!-- Meta Data -->
+    <title>Tylr Creative</title>
+    <meta charset="utf-8" />
+    <meta name="description" content="Brand Identity design by Tyler Robinson." />
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="apple-touch-icon" sizes="57x57" href="/img/favicon/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/img/favicon/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/img/favicon/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/img/favicon/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/img/favicon/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/img/favicon/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/img/favicon/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/img/favicon/apple-touch-icon-152x152.png">
+    <link rel="icon" type="image/png" href="/img/favicon/favicon-196x196.png" sizes="196x196">
+    <link rel="icon" type="image/png" href="/img/favicon/favicon-160x160.png" sizes="160x160">
+    <link rel="icon" type="image/png" href="/img/favicon/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="/img/favicon/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="/img/favicon/favicon-32x32.png" sizes="32x32">
+    <meta name="msapplication-TileColor" content="#1f1f22">
+    <meta name="msapplication-TileImage" content="/img/favicon/mstile-144x144.png">
+
+    <link href="http://dimsemenov-static.s3.amazonaws.com/dist/magnific-popup.css" rel="stylesheet">
+    <!-- Fonts-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,300,900' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href="/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link href="http://tylrcreative.com/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://tylrcreative.com/css/owl.carousel.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="http://tylrcreative.com/css/preloader.css" />
+
+    <link href="http://tylrcreative.com/style.css" rel="stylesheet">
+    <link href="http://tylrcreative.com/css/responsive.css" rel="stylesheet">
+
+    <script src="http://tylrcreative.com/js/modernizr.custom.js"></script>
+</head>
+
+<body id="bigWrapper" data-spy="scroll" data-target=".navbar-default" data-offset="0">
+
+    <header>
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation" data-spy="affix" data-offset-top="0">
+
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="http://tylrcreative.com/">Tylr Creative</a>
+                </div>
+
+                <div class="collapse navbar-collapse navbar-right" id="navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a data-scroll href="http://tylrcreative.com/">Home</a>
+                        </li>
+                        <li><a data-scroll href="http://tylrcreative.com/#about">About</a>
+                        </li>
+                        <li><a data-scroll href="http://tylrcreative.com/portfolio">Portfolio</a>
+                        </li>
+                        <li><a data-scroll href="http://tylrcreative.com/#contact">Contact</a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Downloads <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a data-scroll href="http://tylrcreative.comTylerResume'15.pdf" target="_blank">Resume</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                    </ul>
+                </div>>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+    </header>
+
+    <!--preloader-->
+
+    <div class="ip-header">
+        <div class="ip-logo">
+            <img class="img-responsive preloaderLogo center-block" src="http://tylrcreative.com/img/TR-Logo.svg" alt="preloader">
+        </div>
+
+        <div class="ip-loader">
+            <svg class="ip-inner" width="30px" height="30px" viewBox="0 0 80 80">
+                <path class="ip-loader-circlebg" d="M40,10C57.351,10,71,23.649,71,40.5S57.351,71,40.5,71 S10,57.351,10,40.5S23.649,10,40.5,10z" />
+                <path id="ip-loader-circle" class="ip-loader-circle" d="M40,10C57.351,10,71,23.649,71,40.5S57.351,71,40.5,71 S10,57.351,10,40.5S23.649,10,40.5,10z" />
+            </svg>
+        </div>
+    </div>
+
+    <!--end preloader-->
+
+    <section id="singlePage">
+
+        <section id="singlePageIntro">
+            <div class="container">
+                <div class="row text-center" data-scroll-reveal="enter top move 30px over 1s after 0.2s">
+                    <br>
+                    <br>
+                    <br>
+                    <h1>Brand Identities</h1>
+                    <p>
+                        Graphic Design / Art Direction
+                    </p>
+                    <br>
+                    <br>
+                    <a class="btn btn-default btn-black" href="http://tylrcreative.com/portfolio">
+                        Back to Portfolio
+                    </a>
+                    <div class="clearfix"></div>
+
+                </div>
+            </div>
+        </section>
+
+
+        <!--section-->
+        <section id="singlePageContent" class="white">
+            <div class="container">
+
+                <!--content-->
+                <div class="content" data-scroll-reveal="enter top move 30px over 1s after 0.2s">
+
+                    <!--  info -->
+                    <div class="singleProjectInfo" data-scroll-reveal="enter right move 30px over 1s after 0.2s" style="text-align: center">
+
+                        <span class="fa fa-clock-o"></span>&nbsp;&nbsp;&nbsp;<span class="bold">2011-2014</span>
+
+                    </div>
+                    <!--  end info -->
+
+
+                </div>
+                <!--end content-->
+                <!--  Gallery -->
+
+                <a href="http://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Anthochaera_chrysoptera.jpg/800px-Anthochaera_chrysoptera.jpg" class="without-caption image-link">
+                  <img src="http://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Anthochaera_chrysoptera.jpg/800px-Anthochaera_chrysoptera.jpg" width="172" height="115" />
+                </a>
+                <br/>
+                <a href="http://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Water_Dolphin.jpg/800px-Water_Dolphin.jpg" data-source="http://commons.wikimedia.org/wiki/Commons:Picture_of_the_day" class="with-caption image-link" title="The caption">
+                  <img src="http://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Water_Dolphin.jpg/800px-Water_Dolphin.jpg" width="172" height="105" />
+                </a>
+
+
+                <!--  end Gallery -->
+
+            </div>
+            <!-- /container -->
+
+        </section>
+        <!--end section-->
+
+    </section>
+
+    <section id="footer" class="dark">
+        <div class="bottomLine text-center">
+
+            <ul class="footerSocialIcons">
+                <li><a href="https://www.facebook.com/tylrcreative"><i class="fa fa-facebook fa-2x"></i></a>
+                </li>
+                <li><a href="https://twitter.com/tylr_r"><i class="fa fa-twitter fa-2x"></i></a>
+                </li>
+                <li><a href="http://instagram.com/tylr_r"><i class="fa fa-instagram fa-2x"></i></a>
+                </li>
+                <li><a href="https://www.behance.net/tylr_r"><i class="fa fa-behance fa-2x"></i></a>
+                </li>
+            </ul>
+            <p class="bold">All works © Tyler Robinson 2012 - 2016. </p>
+            <p>
+                <br> Please do not reproduce without the expressed written consent of Tyler Robinson.</p>
+        </div>
+    </section>
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script type="text/javascript" src="http://tylrcreative.com/js/scripts.js"></script>
+    <script src="http://tylrcreative.com/js/custom.js"></script>
+    <script src="http://tylrcreative.com/js/overlay.js"></script>
+    <script src="http://tylrcreative.com/js/preloader.js"></script>
+    <script>
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-56635839-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+
+    <!--Galler JS-->
+    <script>http://dimsemenov-static.s3.amazonaws.com/dist/jquery.magnific-popup.min.js</script>
+    <script>
+    $('.without-caption').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+    $('.with-caption').magnificPopup({
+    		type: 'image',
+    		closeOnContentClick: true,
+    		closeBtnInside: false,
+    		mainClass: 'mfp-with-zoom mfp-img-mobile',
+    		image: {
+    			verticalFit: true,
+    			titleSrc: function(item) {
+    				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+    			}
+    		},
+    		zoom: {
+    			enabled: true
+    		}
+    	});
+    </script>
+
+    <!--Galler JS-->
+
+
+
+</body>
+
+</html>
