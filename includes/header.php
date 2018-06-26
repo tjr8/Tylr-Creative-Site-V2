55a495aad53d0a3c071510124e7b5cbd5f44849b
tjr8 <tjrdesigner@gmail.com>
2018-06-25 23:10:55 -0700

    Update git with last year's changes

---
+++ includes/header.php
@@ -0,0 +1,41 @@
+<header>
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation" data-spy="affix"
+    data-offset-top="50">
+
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Tylr Creative</a>
+            </div>
+
+            <div class="collapse navbar-collapse navbar-right" id="navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a data-scroll href="#intro">Home</a>
+                    </li>
+                    <li><a data-scroll href="#about">About</a>
+                    </li>
+                    <li><a data-scroll href="portfolio.html">Portfolio</a>
+                    </li>
+                    <li><a data-scroll href="#contact">Contact</a>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Downloads <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a data-scroll href="TylerResume'15.pdf" target="_blank">Resume</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+</header>
